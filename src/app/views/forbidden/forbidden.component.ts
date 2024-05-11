294aea0d3fd1820d4d0d7745cae4b402bf70f3a6
Angir777 <90105508+Angir777@users.noreply.github.com>
2024-05-11 14:01:15 +0200

    error and forbidden pages

--- src/app/views/forbidden/forbidden.component.ts
+++ src/app/views/forbidden/forbidden.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-forbidden',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule,
+    RouterModule
+  ],
   templateUrl: './forbidden.component.html',
   styleUrl: './forbidden.component.scss'
 })
--- src/app/views/notfound/notfound.component.ts
+++ src/app/views/notfound/notfound.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule,
+    RouterModule
+  ],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.scss'
 })
